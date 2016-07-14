174995e3870687d8c3f39c854205f0a6c3907795
Jothimani.R <jothimani1991@gmail.com>
2016-07-14 11:27:22 +0530

    Added two modules

---
+++ load_products_on_scroll/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'eCommerce: Load Products on Scroll',
+    'version': '1.0',
+    'category': 'Website',
+    'description': """
+    This module will give you the possibility to load products on scroll in eCommerce.
+
+    """,
+    'author': 'Jothimani R.',
+    'website': ' http://www.jothimani.com',
+    'depends': ['website_sale'],
+    'data': ['views/templates.xml'],
+    'installable': True,
+    'active': False,
+    'price': 100,
+    'currency': 'EUR',
+    'images': ['images/main_screenshot.png'],
+}
