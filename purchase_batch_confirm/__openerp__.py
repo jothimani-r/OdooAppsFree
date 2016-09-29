708a3c473fd8332e857dc65abb1ac0ac8bde802e
Jothimani R <jothimani1991@gmail.com>
2016-09-29 18:12:45 +0530

    [IMP] Finished functionalities of those two new modules

---
+++ purchase_batch_confirm/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Purchase Orders Batch Confirm",
+    'summary': " ",
+    'description': """
+    """,
+    'author': "Jothimani R",
+    'website': "www.linkedin.com/in/rjothimani",
+    'category': 'Purchase Management',
+    'version': '0.1',
+    'depends': ['purchase'],
+    'data': [
+        'wizard/purchase_order_batch_confirm.xml'
+    ],
+}
---
+++ sale_batch_confirm/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Orders Batch Confirm",
+    'summary': " ",
+    'description': """
+    """,
+    'author': "Jothimani R",
+    'website': "www.linkedin.com/in/rjothimani",
+    'category': 'Sales Management',
+    'version': '0.1',
+    'depends': ['sale'],
+    'data': [
+        'wizard/sale_order_batch_confirm.xml'
+    ],
+}
