b00ac0d85837bbe3e5b6aae44a00543320c9a1f4
Jothimani R <jothimani1991@gmail.com>
2016-09-30 18:26:15 +0530

    [IMP] added extra fields in website form

--- hr_recruitment_extended/controllers/main.py
+++ hr_recruitment_extended/controllers/main.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
 from openerp import http
+from openerp.addons.website_hr_recruitment.controllers.main import website_hr_recruitment as Home
+from openerp.http import request
 
-# class HrOnlineJobs(http.Controller):
-#     @http.route('/hr_recruitment_extended/hr_recruitment_extended/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
 
-#     @http.route('/hr_recruitment_extended/hr_recruitment_extended/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('hr_recruitment_extended.listing', {
-#             'root': '/hr_recruitment_extended/hr_recruitment_extended',
-#             'objects': http.request.env['hr_recruitment_extended.hr_recruitment_extended'].search([]),
-#         })
+class WebsiteHrRecruitment(Home):
+    @http.route('/jobs/apply/<model("hr.job"):job>', type='http', auth="public", website=True)
+    def jobs_apply(self, job):
+        error = {}
+        default = {}
+        env = request.env(context=dict(request.env.context, show_address=True, no_tag_br=True))
 
-#     @http.route('/hr_recruitment_extended/hr_recruitment_extended/objects/<model("hr_recruitment_extended.hr_recruitment_extended"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('hr_recruitment_extended.object', {
-#             'object': obj
-#         })
+        State = env['res.country.state']
+        Country = env['res.country']
+        countries_ = Country.search([]).ids
+        countries = Country.sudo().browse(countries_)
+        states_ = State.search([]).ids
+        states = State.sudo().browse(states_)
+
+        if 'website_hr_recruitment_error' in request.session:
+            error = request.session.pop('website_hr_recruitment_error')
+            default = request.session.pop('website_hr_recruitment_default')
+        return request.render("website_hr_recruitment.apply", {
+            'job': job,
+            'error': error,
+            'default': default,
+            'countries': countries,
+            'states': states,
+        })
